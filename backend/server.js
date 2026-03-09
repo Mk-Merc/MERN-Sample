b61ca8dc20ae224463c4e119b2b7f2b054d2154b
Mk-Merc <mkmercado2829@gmail.com>
2026-03-09 23:14:15 +0800

    Updated server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 // Create an instance of the Express application
 const app = express();
 
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 }); 
