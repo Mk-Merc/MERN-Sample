40b598d0e1296cccd7b31693704a65908db5b580
Mk-Merc <mkmercado2829@gmail.com>
2026-03-10 00:45:58 +0800

    Updated server and Added notes

---
+++ backend/routes/notes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Define a route for the root URL
+router.get('/', (req, res) => {
+    
+});
+
+module.exports = router;
+
+// GET notes
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Notes API!' });
+});
+
+// GET single note
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `You requested node with ID: ${id}` });
+});
+
+// POST create a new note
+router.post('/', (req, res) => {
+    res.json({ message: 'Note created successfully!' });
+});
+
+// DELETE a note
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Note with ID: ${id} deleted successfully!` });
+});
+
+// UPDATE a note
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Note with ID: ${id} updated successfully!` });
+});
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 
+const notesRoutes = require('./routes/notes');
+
 // Create an instance of the Express application
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// Remove Basic Route to Test the Server
+
+// Use the notes routes
+app.use('/api/notes', notesRoutes);
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
